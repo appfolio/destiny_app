a7daff3ab783f456a753a7747b43cd399f7fedda
Mark Yanaros <mark.yanaros@appfolio.com>
2015-05-28 13:19:07 -0700

    my cleaned up logging.

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -5,12 +5,8 @@ end
 class ActiveRecord::ConnectionAdapters::AbstractAdapter
   alias oldlog log
 
-  @@counter = 0
   def log sql, *args, &block
     $last_sql = sql unless sql.include? "SHOW"
-    @@counter+=1
-
-    puts "#{@@counter}: #{$last_sql}"
 
     oldlog sql, *args, &block
   end
