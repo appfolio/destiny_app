85ecb416d349abfd34c6b174cc5b430f92a3d669
Mark Yanaros <mark.yanaros@appfolio.com>
2015-07-01 09:27:34 -0700

    my: removed test for remote authentication to login app.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,4 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def test_after_remote_authentication
-    user = User.new
-
-    remote_user = stub(:first_name => 'Donnie', :last_name => 'San', :user_roles => '')
-    user.after_remote_authentication(remote_user)
-
-    assert_equal 'Donnie San', user.name
-  end
 end
