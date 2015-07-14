35320e5b37b7f26c52f1ef9c1b06bd7187ec5d40
Mark Yanaros <mark.yanaros@appfolio.com>
2015-07-14 15:16:16 -0700

    my: Guard password is passed in from environment for prod.

--- lib/gate_guard.rb
+++ lib/gate_guard.rb
@@ -1,13 +1,14 @@
 module GateGuard
   def self.read_letter request, tables_prefix
     session = Capybara::Session.new(:poltergeist)
+    password = Rails.env.production? ? ENV["GUARD_PASS"] : "guardafiymecappin"
     url = "#{request.protocol}#{request.host}:#{request.port}"+"/"
 
     #Find the Guard with the same tables prefix
     unless User.where(name: "Guard", tables_prefix: tables_prefix).present?
       guard = User.new({
         email:"guard@guard.com",
-        password: "guardafiymecappin",
+        password: password,
         name: "Guard",
         tables_prefix: tables_prefix,
         confirmed_at: "2015-05-03 00:00:00"
@@ -18,7 +19,7 @@ module GateGuard
     Thread.new do
       session.visit url
       session.fill_in(:user_email, with: "guard@guard.com")
-      session.fill_in(:user_password, with: "guardafiymecappin")
+      session.fill_in(:user_password, with: password)
       session.click_button("Log in")
 
       url = "#{request.protocol}#{request.host}:#{request.port}"+"/castle/read_letters"
