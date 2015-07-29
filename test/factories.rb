e707eaaddde8234803a6a5a45bcae0bc78fca7bd
Mark Yanaros <mark.yanaros@appfolio.com>
2015-07-29 09:07:43 -0700

    my: Updated tests.

--- test/factories.rb
+++ test/factories.rb
@@ -1,16 +1,18 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, class: User do
     name "Bobbbbbbb user"
     email "person@appfolio.com"
-    encrypted_password "password"
+    password "password"
     password_confirmation "password"
+    confirmed_at "2015-07-29 00:04:34"
   end
 
   factory :user_with_tables_prefix, class: User do
     name "Cool user"
     email "im@appfolio.com"
-    encrypted_password "password"
+    password "password"
     password_confirmation "password"
+    confirmed_at "2015-07-29 00:04:34"
     tables_prefix "123456788"
   end
 end
